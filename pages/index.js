47535eee5ddc285305e952306fba7596d6041af9
whdghks <johjhj0530@fount.co>
2023-12-29 16:53:16 +0900

    main 페이지 추가

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,29 @@
-function Home() {
-    return <div>Welcome to Next.js!</div>
+// pages/MainPage.js
+
+import Link from "next/link";
+import Image from 'next/image';
+import styles from "../src/main.module.css"; // CSS 모듈 import
+
+export default function MainPage() {
+    return (
+      <div className={styles.mainPage}>
+        <div className={styles.contentWrapper}>
+          <div className={styles.centerContent}>
+            {/* 이미지 추가 */}
+            <div className={styles.imageWrapper}>
+              <Image src="/image/logo.jpeg" alt="Logo" width={200} height={200} />
+            </div>
+            {/* 버튼 */}
+            <div className={styles.buttonWrapper}>
+              <Link href="/team">
+                <div className={styles.button}>팀 배정</div>
+              </Link>
+              <Link href="/mack">
+                <div className={styles.button}>막뽑기</div>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
-  
-  export default Home
